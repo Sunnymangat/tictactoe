cc3f1bd6b24a6df8b7b27a306f3146f0b481f8d6
Sunnymangat <jaskaran887200@gmail.com>
2021-08-30 21:01:26 +0530

    board and square components are created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React from 'react';
+import Board from './component/Board';
 
-export default () => (
-  <>
-    <h1>Welcome to React Parcel Micro App!</h1>
-    <p>Why Jatta??</p>
-  </>
-);
+const App = () => {
+  return (
+    <div>
+      <h1>TIC TAC TOE</h1>
+      <Board />
+    </div>
+  );
+};
+export default App;
